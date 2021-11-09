7a763d5a5199b9b9f26f90e116e05a2c59b68fdb
manelnegra <manelnegra@github.com>
2021-11-09 01:19:30 +0100

    firstInterfacesCommit

---
+++ app/src/main/java/com/example/hworkarranger/HomeChefActivity2.java
@@ -0,0 +1,14 @@
+package com.example.hworkarranger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeChefActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_chef2);
+    }
+}
